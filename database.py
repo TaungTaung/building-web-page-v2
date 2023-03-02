bb3d655d3dcd18756f774e07be6ed07e91bdb038
taungkun thumsattaya <125723692+TaungTaung@users.noreply.github.com>
2023-03-02 16:47:36 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -17,3 +17,15 @@ def load_jobs_from_db():
     for dict_row in result.mappings():
       jobs.append(dict(dict_row))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"),{"val":id})
+    jobs = []
+    for dict_row in result.mappings():
+      jobs.append(dict(dict_row))
+      
+    if len(jobs) == 0:
+      return None
+    else:
+      return jobs[0]
