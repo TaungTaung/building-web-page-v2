0aaa8001100a89554bb67a334193435406a501de
taungkun thumsattaya <125723692+TaungTaung@users.noreply.github.com>
2023-03-06 08:01:42 +0000

    store application in database

--- database.py
+++ database.py
@@ -23,9 +23,21 @@ def load_job_from_db(id):
     result = conn.execute(text("select * from jobs where id = :val"),{"val":id})
     jobs = []
     for dict_row in result.mappings():
-      jobs.append(dict(dict_row))
+      jobs.append(dict_row)
       
     if len(jobs) == 0:
       return None
     else:
-      return jobs[0]
+      return dict(jobs[0])
+
+def add_application_to_db(job_id,data):
+  with engine.connect() as conn:
+    conn.execute(text("insert into applications (job_id, full_name, email, linkedin_url, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :work_experience, :resume_url)"),
+                 {
+                   "job_id": job_id,
+                   "full_name":data['full_name'],
+                   "email":data['email'],
+                   "linkedin_url":data['linkedin_url'],
+                   "work_experience":data['work_experience'],
+                   "resume_url":data['resume_url']
+                 })
